4d1d97f520f606ebdf8ca13a37b851f7a45c1c7b
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2015-06-05 10:18:44 -0500

    fixing Consul DNS routing issues

--- templates/upstart.js
+++ templates/upstart.js
@@ -17,10 +17,13 @@ script
     docker rm ${UPSTART_JOB}
   fi
 
+  # Use "--rm" rather than "--restart" when testing
+
   /usr/bin/docker run \
     --name ${UPSTART_JOB} \
     -h `cat /etc/hostname` \
     --restart="always" \
+    --dns=172.17.42.1 \
     -p 9000:9000 \
     {{kafka_manager_container}}:{{kafka_manager_version}}
 
